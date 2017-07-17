9df83516e04f0c687dc3ac313e89846347d7299d
ruixue0702 <ruixue0702@163.com>
2017-07-17 22:39:01 +0800

    Add files via upload

---
+++ js/slides.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper ('#swiper1', {
+    autoplay: 5000,//可选选项，自动滑动
+    autoplayDisableOnInteraction : false,
+    loop: true,
+
+    // 如果需要前进后退按钮
+    // nextButton: '.swiper-button-next',
+    // prevButton: '.swiper-button-prev',
+
+})        
